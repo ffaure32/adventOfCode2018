769bfd1babf615c481b8b17eec25144ffa5e9b5b
ffaure <frederic.faure@ext.cdiscount.com>
2018-12-04 09:52:40 +0100

    Refacto day 3 and part 2 implementation

--- src/test/java/DayThreeTest.java
+++ src/test/java/DayThreeTest.java
@@ -1,3 +1,5 @@
+import model.Claim;
+import model.ClaimGrid;
 import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Test;
@@ -5,17 +7,18 @@ import org.junit.Test;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
 public class DayThreeTest {
+
+    public static final ArrayList<String> SAMPLE = Lists.newArrayList("#1 @ 1,3: 4x4",
+            "#2 @ 3,1: 4x4",
+            "#3 @ 5,5: 2x2");
+    public static final String INPUT_DAY_3_FILE_NAME = "inputDay3.txt";
+
     @Test
     public void testPattern() {
         Claim claim = Claim.toClaim("#123 @ 3,2: 5x4");
@@ -27,47 +30,46 @@ public class DayThreeTest {
     }
 
     @Test
-    public void testWithSample() {
-        List<String> sample = Lists.newArrayList("#1 @ 1,3: 4x4",
-                "#2 @ 3,1: 4x4",
-                "#3 @ 5,5: 2x2");
-        List<Claim> claims = sample.stream().map(cl -> Claim.toClaim(cl)).collect(Collectors.toList());
-        Optional<Claim> maxWidth = claims.stream().max(Comparator.comparing(cl -> cl.totalWidth()));
-        Optional<Claim> maxHeight = claims.stream().max(Comparator.comparing(cl -> cl.totalHeight()));
-        int width =  maxWidth.get().totalWidth();
-        int height = maxHeight.get().totalHeight();
+    public void testPartOneWithSample() {
+        List<String> sample = SAMPLE;
 
-        int array[][] = new int[width][height];
-
-        claims.stream().forEach(cl -> fillArray(array, cl));
-        int cells = 0;
-        for (int[] line : array) {
-            cells += Arrays.stream(line).filter(i -> i>1).count();
-        }
+        DayThree dayThree = new DayThree(sample);
+        int cells = dayThree.countOverlappingCells();
         assertEquals(4, cells);
     }
 
     @Test
-    public void realTest() throws Exception {
+    public void testPartTwoWithSample() {
+        List<String> sample = SAMPLE;
+
+        DayThree dayThree = new DayThree(sample);
+        Claim uniqueClaim = dayThree.getUniqueClaim();
+        assertEquals(3, uniqueClaim.id);
+    }
+
+    @Test
+    public void testRealPartOne() throws Exception {
         Path path = Paths.get(getClass().getClassLoader()
-                .getResource("inputDay3.txt").toURI());
+                .getResource(INPUT_DAY_3_FILE_NAME).toURI());
 
         List<String> lines = Files.readAllLines(path);
-        List<Claim> claims = lines.stream().map(cl -> Claim.toClaim(cl)).collect(Collectors.toList());
-        Optional<Claim> maxWidth = claims.stream().max(Comparator.comparing(cl -> cl.totalWidth()));
-        Optional<Claim> maxHeight = claims.stream().max(Comparator.comparing(cl -> cl.totalHeight()));
-        int width =  maxWidth.get().totalWidth();
-        int height = maxHeight.get().totalHeight();
-
-        int array[][] = new int[width][height];
-
-        claims.stream().forEach(cl -> fillArray(array, cl));
-        int cells = 0;
-        for (int[] line : array) {
-            cells += Arrays.stream(line).filter(i -> i>1).count();
-        }
+        DayThree dayThree = new DayThree(lines);
+        int cells = dayThree.countOverlappingCells();
         assertEquals(116140, cells);
     }
+
+    @Test
+    public void testPartThreeWithSample() throws Exception {
+        Path path = Paths.get(getClass().getClassLoader()
+                .getResource(INPUT_DAY_3_FILE_NAME).toURI());
+
+        List<String> lines = Files.readAllLines(path);
+
+        DayThree dayThree = new DayThree(lines);
+        Claim uniqueClaim = dayThree.getUniqueClaim();
+        assertEquals(574, uniqueClaim.id);
+    }
+
 
     private void fillArray(int[][] array, Claim cl) {
         for (int i = cl.leftEdge; i < cl.leftEdge+cl.width; i++) {
