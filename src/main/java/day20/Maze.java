f6cf910d42f1a49fe7f5e62be7f0322a361f846c
ffaure32 <ffaure32@yahoo.fr>
2018-12-26 08:08:37 +0100

    beginning of day 20

--- src/main/java/day20/Maze.java
+++ src/main/java/day20/Maze.java
@@ -39,12 +39,14 @@ public class Maze {
     public Map<Position, Route> maze = new HashMap<>();
 
     private Set<String> pathes;
+
     public Maze(String input) {
-        pathes = cutInput(input.substring(1, input.length()-1));
+        // pathes = cutInput(input.substring(1, input.length() - 1));
+        drawMazeDirectly(new Position(0,0), input.substring(1, input.length() - 1));
     }
 
     public void drawMaze() {
-        for(String path: pathes) {
+        for (String path : pathes) {
             System.out.println(path);
             System.out.println(path.length());
             Position currentPosition = new Position(0, 0);
@@ -82,11 +84,77 @@ public class Maze {
         }
     }
 
+    public void drawMazeDirectly(Position position, String input) {
+        int firstParenthesis = input.indexOf("(");
+
+        if (firstParenthesis >= 0) {
+            Position newPosition = drawMazePart(position, input.substring(0, firstParenthesis));
+            int close = findClosing(input, firstParenthesis);
+            String parenthesisContent = input.substring(firstParenthesis + 1, close);
+            int separator = Maze.findSeparator(parenthesisContent);
+            String firstPart = parenthesisContent.substring(0, separator);
+            String secondPart = parenthesisContent.substring(separator + 1);
+            String firstInput = firstPart + input.substring(close + 1);
+            drawMazeDirectly(newPosition, firstInput);
+            String secondInput = secondPart + input.substring(close + 1);
+            drawMazeDirectly(newPosition, secondInput);
+        } else {
+            int separator = Maze.findSeparator(input);
+            if(separator>= 0) {
+                String firstPart = input.substring(0, separator);
+                String secondPart = input.substring(separator + 1);
+                drawMazeDirectly(position, firstPart);
+                drawMazeDirectly(position, secondPart);
+            } else {
+                drawMazePart(position, input);
+            }
+        }
+
+    }
+
+    public Position drawMazePart(Position currentPosition, String todraw) {
+
+        if(currentPosition.equals(new Position(0,0))) {
+            maze.put(currentPosition, Route.START);
+        }
+
+        for (char c : todraw.toCharArray()) {
+            Direction dir = Direction.from(c);
+            switch (dir) {
+                case WEST:
+                    currentPosition = currentPosition.left();
+                    maze.put(currentPosition, Route.VDOOR);
+                    currentPosition = currentPosition.left();
+                    maze.put(currentPosition, Route.PATH);
+                    break;
+                case EAST:
+                    currentPosition = currentPosition.right();
+                    maze.put(currentPosition, Route.VDOOR);
+                    currentPosition = currentPosition.right();
+                    maze.put(currentPosition, Route.PATH);
+                    break;
+                case NORTH:
+                    currentPosition = currentPosition.up();
+                    maze.put(currentPosition, Route.HDOOR);
+                    currentPosition = currentPosition.up();
+                    maze.put(currentPosition, Route.PATH);
+                    break;
+                case SOUTH:
+                    currentPosition = currentPosition.down();
+                    maze.put(currentPosition, Route.HDOOR);
+                    currentPosition = currentPosition.down();
+                    maze.put(currentPosition, Route.PATH);
+                    break;
+            }
+        }
+        return currentPosition;
+    }
+
     public Set<String> cutInput(String input) {
         Set<String> newResults = new HashSet<>();
         Set<String> result = getInputs(input);
-        if(result.size()> 1) {
-            for(String newResult : result) {
+        if (result.size() > 1) {
+            for (String newResult : result) {
                 newResults.addAll(cutInput(newResult));
             }
         } else {
@@ -98,14 +166,16 @@ public class Maze {
     @NotNull
     private Set<String> getInputs(String input) {
         Set<String> result = new HashSet<>();
-        int firstParenthesis  = input.indexOf("(");
-        if(firstParenthesis>=0) {
+        int firstParenthesis = input.indexOf("(");
+        if (firstParenthesis >= 0) {
             int close = findClosing(input, firstParenthesis);
-            String parenthesisContent = input.substring(firstParenthesis+1, close);
-            String[] split = parenthesisContent.split("\\|", 2);
-            String firstInput = input.substring(0, firstParenthesis)+split[0]+input.substring(close+1);
+            String parenthesisContent = input.substring(firstParenthesis + 1, close);
+            int separator = Maze.findSeparator(parenthesisContent);
+            String firstPart = parenthesisContent.substring(0, separator);
+            String secondPart = parenthesisContent.substring(separator + 1);
+            String firstInput = input.substring(0, firstParenthesis) + firstPart + input.substring(close + 1);
             result.add(firstInput);
-            String secondInput = input.substring(0, firstParenthesis)+split[1]+input.substring(close+1);
+            String secondInput = input.substring(0, firstParenthesis) + secondPart + input.substring(close + 1);
             result.add(secondInput);
         } else {
             result.add(input);
@@ -127,6 +197,7 @@ public class Maze {
             }
         }
     }
+
     public static int findClosing(String s, int n) {
 
         int counter = 0;
@@ -134,14 +205,13 @@ public class Maze {
         char closing = ')';
         int positionOfMatchingParen = -1;
         boolean found = false;
-        while(n < s.length() && !found) {
+        while (n < s.length() && !found) {
 
-            if(s.charAt(n) == (opening)) {
+            if (s.charAt(n) == (opening)) {
                 counter++;
-            }
-            else if(s.charAt(n) == (closing)) {
+            } else if (s.charAt(n) == (closing)) {
                 counter--;
-                if(counter == 0) {
+                if (counter == 0) {
                     positionOfMatchingParen = n;
                     found = true;
                 }
@@ -150,4 +220,29 @@ public class Maze {
         }
         return positionOfMatchingParen;
     }
+
+
+    public static int findSeparator(String s) {
+        int n = 0;
+        int counter = 0;
+        char opening = '(';
+        char closing = ')';
+        int positionOfMatchingParen = -1;
+        boolean found = false;
+        while (n < s.length() && !found) {
+
+            if (s.charAt(n) == (opening)) {
+                counter++;
+            } else if (s.charAt(n) == (closing)) {
+                counter--;
+            } else if (s.charAt(n) == '|') {
+                if (counter == 0) {
+                    return n;
+                }
+            }
+            n++;
+        }
+        return positionOfMatchingParen;
+    }
+
 }
